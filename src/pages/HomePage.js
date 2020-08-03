3d390dc828f83e23b21a6612200a8b70842ada95
arb1ona <arbiona.shahu@gmail.com>
2020-08-03 23:39:48 +0200

    HomePage added

---
+++ src/pages/HomePage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { LoremIpsum } from 'react-lorem-ipsum';
+
+const HomePage = () => (
+    <>
+        <h1>Welcome to my blog</h1>
+    </>
+)
+
+export default HomePage
